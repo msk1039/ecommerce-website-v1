d367f71d7c1753b4c6677a6a54a7d5939790cc67
Mayank kadam <mayankkadam1039@gmail.com>
2024-06-30 03:01:59 +0530

    actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/ecommerce-website-v1",
+  output: "export", 
     images: {
         domains: ['picsum.photos','placehold.jp','www.aristobrat.in']
       }
